94e3b90bf9706a0569d43a8bf4efb99fb8caa652
aaa71541367 <69131671+aaa71541367@users.noreply.github.com>
2020-08-03 14:48:10 +0800

    Add files via upload

---
+++ day 1-2.py
@@ -0,0 +1,7 @@
+name=input('你叫三小')
+
+print("Hi "+name)
+
+    
+
+
---
+++ day 1-5.py
@@ -0,0 +1,20 @@
+分數=input('分數 :')
+分數=int(分數)
+if 分數>0 and 分數<100:
+   if 分數>=90:
+      print('A')
+   elif 分數>=80:
+        print('B')
+   elif 分數>=70:
+        print('C')
+   elif 分數>=60:
+        print('D')
+   else :
+        print('E')
+else :
+    print('錯誤')
+    
+    
+    
+    
+    
